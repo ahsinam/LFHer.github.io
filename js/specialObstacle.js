afb8cbe0e36b46f9b45745686788ed91a34f8f89
ahsinam171 <ahsinam171@gmail.com>
2023-05-29 21:33:54 +0545

    special Object

---
+++ js/specialObstacle.js
@@ -0,0 +1,24 @@
+class SpecialObstacle {
+  constructor(ctx, x, y, width, height, wood, ladder) {
+    this.ctx = ctx;
+    this.specialObsXpos = x;
+    this.specialObsYpos = y;
+    this.specialObsWidth = width;
+    this.specialObsHeight = height;
+    this.wood = wood;
+    this.ladder = ladder;
+
+    this.specialObsImage = new Image();
+    this.specialObsImage.src = "../Images/obstacle/obstacleBottleFall.png";
+  }
+
+  drawSpecialObstacle() {
+    this.ctx.drawImage(
+      this.specialObsImage,
+      this.specialObsXpos,
+      this.specialObsYpos,
+      this.specialObsWidth,
+      this.specialObsHeight
+    );
+  }
+}
